77bebd9f7416428eedb7c21b4d2d81d0bb9b1a12
n8blake <nblake@middleburyinteractive.com>
2012-08-30 15:55:36 -0600

    Updating the php pages. Added mysql database query functions.

--- attendance_php.php
+++ attendance_php.php
@@ -1,33 +1,34 @@
 <?php
-$con = mysql_connect("localhost","admin","qanotify");
-if (!$con)
-  {
-  die('Could not connect: ' . mysql_error());
-  }
+require("../credentials.php");
+if ($userName && $dbName && $hostName) {
+	
+	$con = mysql_connect($hostName, $userName, $password);
+	if($con){
+		print("Connected.");
+		
+		// Execute query
+		
+		// $sql = "SELECT * FROM `swimmers`";
+		
+		$mysqli = new mysqli($hostName, $userName, $password, $dbName);
+		$result = $mysqli->query("SELECT * FROM `swimmers`");		
+		$swimmersArray = array($result);
+		print_r ($result);
+		mysql_close($con);
+	}
+	
+	if (!$con)
+	  {
+	  die('Could not connect: ' . mysql_error());
+	  }
+	
 
-// Create database
-if (mysql_query("CREATE DATABASE swimmers",$con))
-  {
-  echo "Database created";
-  }
-else
-  {
-  echo "Error creating database: " . mysql_error();
-  }
+	
 
-// Create table
-mysql_select_db("swimmers", $con);
-$sql = "CREATE TABLE Persons
-(
-personID int NOT NULL AUTO_INCREMENT, 
-PRIMARY KEY(personID),
-FirstName varchar(15),
-LastName varchar(15),
-Age int
-)";
 
-// Execute query
-mysql_query($sql,$con);
+} else {
+	print("No database credentials.");
+}
 
-mysql_close($con);
 ?>
+
